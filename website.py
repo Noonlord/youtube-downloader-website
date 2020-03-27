1439f6f31069bbf9489852ed830f3aee29a55ef9
Sibyl666 <54271295+Sibyl666@users.noreply.github.com>
2020-03-27 23:21:35 +0300

    htmlzamazingo

--- website.py
+++ website.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from markupsafe import escape
+import requests as req
 app = Flask(__name__)
 
-@app.route('/download/<vidID>')
-def download(vidID):
-    return escape(vidID)
+@app.route('/download/', methods=['POST', 'GET'])
+def download():
+    if request.method == "POST":
+        return render_template('download.html.jinja', ytlink=request.form["ytlink"])
+    else:
+        return render_template('download.html.jinja')
+        
+#https://www.youtube.com/watch?v=oBYbxw8f5OI
