309726621ba2323e0717286e4cb267dcf34a0d78
Noonlord <mymmym22@gmail.com>
2020-04-03 14:05:07 +0300

    support youtu.be links

--- website.py
+++ website.py
@@ -14,7 +14,7 @@ def download():
         os.system("youtube-dl -o \"static/%(id)s\" --format \"bestvideo[height<=480]+bestaudio[ext=m4a]/bestvideo+bestaudio/best\" --merge-output-format mp4 " + ytlink)
         print("stop")
         if "youtu.be" in ytlink:
-            pass
+            id = ytlink.split("/")[1]
         elif "youtube.com" in ytlink:
             id = ytlink.split("=")[1][:11]
             fileUrl = "http://127.0.0.1:5000/static/" + id + ".mp4"
