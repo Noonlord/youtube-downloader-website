44a532c4c37f800964815dcf3f4d3f1fef964996
Noonlord <mymmym22@gmail.com>
2020-04-18 16:52:46 +0300

    download format selection

--- website.py
+++ website.py
@@ -10,17 +10,28 @@ app = Flask(__name__)
 def download():
     ytlink=request.args.get("ytlink", 0)
     if ytlink:
+        print(request.args)
+        format = request.args["format"]
+        print(format)
         print("start")
-        os.system("youtube-dl -o \"static/%(id)s\" --format \"bestvideo[height<=480]+bestaudio[ext=m4a]/bestvideo+bestaudio/best\" --merge-output-format mp4 " + ytlink)
+        #youtube-dl -x --audio-format mp3
+        if format == "mp4":
+            os.system("youtube-dl -o \"static/%(id)s\" --format \"bestvideo[height<=480]+bestaudio[ext=m4a]/bestvideo+bestaudio/best\" --merge-output-format mp4 " + ytlink)
+        elif format == "mp3":
+            os.system("youtube-dl -o \"static/%(id)s.%(ext)s\" -x --audio-format mp3 " + ytlink)
         print("stop")
         if "youtu.be" in ytlink:
             id = ytlink.split("/")[1]
         elif "youtube.com" in ytlink:
             id = ytlink.split("=")[1][:11]
-            fileUrl = "http://127.0.0.1:5000/static/" + id + ".mp4"
-            print(fileUrl)
+        if format == "mp4":
+            vidExt = ".mp4"
+        elif format == "mp3":
+            vidExt = ".mp3"
+        fileUrl = "http://127.0.0.1:5000/static/" + id + vidExt
+        print(fileUrl)
         soup = BeautifulSoup(urllib.request.urlopen(ytlink))
-        videoName = soup.title.string + ".mp4"
+        videoName = soup.title.string + vidExt
         videoThumb = "https://img.youtube.com/vi/" + id + "/mqdefault.jpg"
         return render_template('download.html', downloadLink = fileUrl, videoName = videoName, videoThumb = videoThumb)
     else:
